2e4c3061d089a6931ef452babdae768711b0ad18
Bhanu Pratap Singh Thapliyal <bpst@Bhanus-MacBook-Air.local>
2018-06-08 10:44:45 +0530

    adding Pingpong game structure

--- spriteKitintro/GameScene.swift
+++ spriteKitintro/GameScene.swift
@@ -15,11 +15,14 @@ class GameScene: SKScene {
     var ball = SKSpriteNode()
     var enemy = SKSpriteNode()
     var main = SKSpriteNode()
+    var score : [Int] = []
     
     
     
-    
-    override func didMove(to view: SKView) {
+        override func didMove(to view: SKView)
+        {
+            
+        startGame()
         
         ball = self.childNode(withName: "ball") as!SKSpriteNode
         
@@ -35,11 +38,47 @@ class GameScene: SKScene {
         border.restitution = 1
         
         self.physicsBody = border
+            
+        }
+    
+    
+    
+    
+    
+    /* score calculation    */
+
+    func startGame()
+    {
+        score = [0,0]
         
-       
         
-       
     }
+    func addScore(win :SKSpriteNode)  {
+        
+        ball.position = CGPoint(x : 0,y :0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if win == main
+        {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
+
+            
+        }
+        else if win == enemy {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -30))
+
+        }
+     
+        print(score)
+    }
+    
+    
+    
+    
+    // makes the main player move
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
@@ -60,9 +99,24 @@ class GameScene: SKScene {
         }
     }
     
+    
+    
+    //UPDATE  MAKES the enemy move
+    
     override func update(_ currentTime: TimeInterval) {
         
-         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.7))
+        
+        if (ball.position.y <=  main.position.y - 60)
+        {
+            addScore(win: enemy)
+        }
+        else if (ball.position.y >= enemy.position.y + 60 )
+        {
+            addScore(win: main)
+        }
+        
+        print(score)
         
     }
 }
